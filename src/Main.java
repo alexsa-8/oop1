94b6c1c0abd7f4c27416acf950346f9390151de9
Alex <alexsa-8@mail.ru>
2022-09-23 23:05:24 +0300

    oop1 Домашнее задание-3.2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        car lada = new car("Lada", "Grand", 1.7, "жёлтый",
+        car lada = new car(null, "Grand", 1.7, "жёлтый",
                 2015, "Россия");
-        car audi = new car("Audi", "A8 50 L TDI quattro", 3.0, "черный",
+        car audi = new car("Audi", "A8 50 L TDI quattro",  3.0, "черный",
                 2020, "Германия");
-        car bmw = new car("BMW", "Z8", 3.0, "черный", 2021,
+        car bmw = new car("BMW", "Z8", 3.0, null, 2021,
                 "Германия");
         car kia = new car("Kia", "Sportage 4 поколение", 2.4, "красный",
                 2018, "Южная Корея");
         car hyundai = new car("Hyundai", "Avante", 1.6, "оранжевый",
                 2016, "Южная Корея");
+
+        lada.messageCar();
+        audi.messageCar();
+        bmw.messageCar();
+        kia.messageCar();
+        hyundai.messageCar();
     }
 }
--- src/car.java
+++ src/car.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class car {
     String brand;
     String model;
@@ -6,13 +8,25 @@ public class car {
     int productionYear;
     String productionCountry;
 
-    public car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
-        this.brand=brand;
-        this.model=model;
+    car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
+        this.brand= Objects.requireNonNullElse(brand,"default");
+        this.model=Objects.requireNonNullElse(model,"default");
         this.engineVolume=engineVolume;
-        this.color=color;
+        this.color=Objects.requireNonNullElse(color,"default");
         this.productionYear=productionYear;
-        this.productionCountry=productionCountry;
+        this.productionCountry=Objects.requireNonNullElse(productionCountry,"default");
+    }
+    car(String brand, String model, String color, int productionYear, String productionCountry){
+        this(brand, model, 1.5, color, productionYear, productionCountry);
+    }
+    car(String brand, String model, double engineVolume, int productionYear, String productionCountry){
+        this(brand, model, engineVolume, "белый", productionYear, productionCountry);
+    }
+    car(String brand, String model, double engineVolume, String color, String productionCountry){
+        this(brand, model, engineVolume, color, 2000, productionCountry);
+    }
+
+    void messageCar(){
         System.out.println("Марка: " + brand + ", модель: " + model + ", объём двигателя " + engineVolume + ", цвет кузова: "
                 + color + ", год выпуска: " + productionYear + ", страна сборки: " + productionCountry + ".");
     }
