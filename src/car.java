cbfa0a178baea91e0327d1dcfdf0e83187981b0a
Alex <alexsa-8@mail.ru>
2022-09-23 17:41:22 +0300

    oop1 используем конструктор.

--- src/car.java
+++ src/car.java
@@ -7,6 +7,12 @@ public class car {
     String productionCountry;
 
     public car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
+        this.brand=brand;
+        this.model=model;
+        this.engineVolume=engineVolume;
+        this.color=color;
+        this.productionYear=productionYear;
+        this.productionCountry=productionCountry;
         System.out.println("Марка: " + brand + ", модель: " + model + ", объём двигателя " + engineVolume + ", цвет кузова: "
                 + color + ", год выпуска: " + productionYear + ", страна сборки: " + productionCountry + ".");
     }
